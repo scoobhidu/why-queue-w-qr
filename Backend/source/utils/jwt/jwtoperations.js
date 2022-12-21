318005d45c6e063d3520beb1243f97bedec346c9
Ansh Goyal <110151117+AnshGoyal27@users.noreply.github.com>
2022-12-21 23:07:59 +0530

    Add files via upload

---
+++ Backend/source/utils/jwt/jwtoperations.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+
+const obj = {
+    SECRET: 'Hello',
+    generateLoginToken(userid,password){
+        console.log(process.env.TEACHER_TOKEN);
+        const token = jwt.sign({'userName':userid,'password':password},process.env.TEACHER_TOKEN,{
+            //algorithm:'RS256',
+            expiresIn:'1200s'
+        });
+        console.log('Token ');
+        return token;
+    },
+    decodeLoginToken(tokenId){
+        try{
+        const decode = jwt.verify(tokenId,process.env.TEACHER_TOKEN);
+        console.log(decode);
+        if(decode && decode.userName){
+            console.log(decode.userid);
+            return true;
+        }
+        else{
+            console.log("Invalid Token ");
+            return false;
+        }
+    }
+    catch(err){
+        console.log('Catch Error in Decode::::: ', err);
+        return false;
+    }
+    }
+
+}
+module.exports = obj;
