400417a29b8bd1307fe3679590376c2529cb0376
Rajat Kumar <loyaltytrooper@gmail.com>
2022-12-23 12:05:25 +0530

    [feat] hack ends #2

--- utils/checkJWT.go
+++ utils/checkJWT.go
@@ -3,14 +3,26 @@ package utils
 import (
 	"sync"
 	_ "time"
-	"why-queue-w-qr/models"
 )
 
 var JwtCheck = make(chan bool)
 
+var JwtCollection map[string]int64
+
+func init() {
+	JwtCollection = make(map[string]int64)
+	JwtCollection["jwt7"] = 167177092
+	JwtCollection["jwt1"] = 167177092
+	JwtCollection["jwt2"] = 167177093
+	JwtCollection["jwt3"] = 167177951
+	JwtCollection["jwt4"] = 167172952
+	JwtCollection["jwt5"] = 167172952
+	JwtCollection["jwt6"] = 167172952
+}
+
 func CheckJWT(jwt string, timeStamp int64, wg *sync.WaitGroup) {
 	defer wg.Done()
-	creationTime, ok := models.JwtCollection[jwt]
+	creationTime, ok := JwtCollection[jwt]
 	if ok && creationTime <= timeStamp && timeStamp <= creationTime+10000 {
 		JwtCheck <- true
 	} else {
